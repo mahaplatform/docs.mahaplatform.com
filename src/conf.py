796f1a896a4050b0b624f50d7517f42c4cf78d44
Greg Kops <greg@thinktopography.com>
2021-05-14 18:04:57 -0400

    fixing style of icon

--- src/conf.py
+++ src/conf.py
@@ -108,7 +108,7 @@ html_context = {}
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = [
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
     'css/custom.css'
 ]
 # Custom sidebar templates, must be a dictionary that maps document names
